30909593dd64c45d8dcd2465241657c127e7d76c
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-04-14 10:40:06 +0700

    Membuat penerimaan data

--- LinearSearch_160/LinearSearch_160.cpp
+++ LinearSearch_160/LinearSearch_160.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 
+
 int arr[20]; //jumlah array maksimum 
 int n; // number of element in the array
 int i; // index of array element
+
+
 
 void input() {
     while (true) {
@@ -12,10 +15,21 @@ void input() {
         if ((n > 0) && (n <= 20))
             break;
         else
-            cout << "\nArray minimum adalah 1 dan maksimum adalah 20"
+            cout << "\nArray minimum adalah 1 dan maksimum adalah 20";
     }
+    
+    //Accept array elements
+    cout << "\n=============\n";
+    cout << " Enter Array Elements\n";
+    cout << "\n=============\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+
 }
 int main()
 {
-    std::cout << "Hello World!\n";
+  
 }
