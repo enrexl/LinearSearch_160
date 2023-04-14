25e7224aecc13716580ce20f85e885c76e5dadee
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-04-14 10:33:22 +0700

    Membuat void input

--- LinearSearch_160/LinearSearch_160.cpp
+++ LinearSearch_160/LinearSearch_160.cpp
@@ -5,7 +5,13 @@ int arr[20]; //jumlah array maksimum
 int n; // number of element in the array
 int i; // index of array element
 
-
+void input() {
+    while (true) {
+        cout << "Enter the numner of element in the array = ";
+        cin >> n;
+        
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
