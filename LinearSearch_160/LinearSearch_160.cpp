cabd46acf2c8245ef59bd67f4d424d3641930f12
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-04-14 10:56:38 +0700

    Membuat fungsi Linear Search

--- LinearSearch_160/LinearSearch_160.cpp
+++ LinearSearch_160/LinearSearch_160.cpp
@@ -19,17 +19,47 @@ void input() {
     }
     
     //Accept array elements
-    cout << "\n=============\n";
+    cout << "\n================\n";
     cout << " Enter Array Elements\n";
-    cout << "\n=============\n";
+    cout << "\n================\n";
     for (int i = 0; i < n; i++)
     {
         cout << "<" << (i + 1) << ">";
         cin >> arr[i];
     }
+}
 
+void LinearSearch() {
+    char ch;
+    int comparison; //variable menghitung perbandingan
+
+    do
+    {   //menerima angka yang akan dicari
+        cout << "\nEnter the element that you want to search = "; //langkah 1
+        int item;
+        cin >> item;
+
+        comparison = 0;
+        for (int i = 0; i < n; i++)     //langkah 2,3 dan 4.
+        {
+            comparison++;
+            if (arr[i] == item)         //langkah 5 #Found
+            {
+                cout << "\n" << item << " Found at position" << (i + 1) << endl;
+                break;
+            }
+        }
+        if (arr[i] == n) 
+            cout << "\n" << item << "Not found in the array\n";
+            cout << "\nNumber of comparison = " << comparison << endl;
+
+            cout << "\nContinue search (y/n) : ";
+            cin >> ch;
+    } while ((ch == 'y') || (ch == 'Y'));
 }
 int main()
 {
-  
+    input();
+    LinearSearch();
+    return 0;
 }
